7526edf6d8f80a4dd485619f26988ec1910176ef
Ramsses <43966422+cramsses@users.noreply.github.com>
2021-01-04 15:57:55 -0600

    variables y tipos

--- curso-typescript/prueba.js
+++ curso-typescript/prueba.js
@@ -6,3 +6,16 @@ var resultado = holaMundo(nombre);
 var etiqueta = document.getElementById("container");
 etiqueta.innerHTML = resultado;
 alert("Hola");
+//Variables y tipaos
+var nombre_dos = "Edgar";
+var edad = 37;
+etiqueta.innerHTML = nombre_dos + " - " + edad;
+//let vs var
+var a = 7;
+var b = 12;
+if (a === 7) {
+    var a_1 = 4;
+    var b = 1;
+    console.log("Dentro de IF: " + a_1 + " - " + b);
+}
+console.log("Fuera de IF: " + a + " - " + b);
--- curso-typescript/prueba.ts
+++ curso-typescript/prueba.ts
@@ -7,5 +7,22 @@ var resultado = holaMundo(nombre)
 
 var etiqueta = <HTMLElement> document.getElementById("container");
 etiqueta.innerHTML = resultado;
-
 alert("Hola");
+
+//Variables y tipaos
+var nombre_dos:string = "Edgar";
+var edad:number = 37;
+
+etiqueta.innerHTML = nombre_dos+" - "+edad;
+
+//let vs var
+
+var a=7;
+var b=12
+
+if(a===7){
+    let a = 4;
+    var b = 1;
+    console.log("Dentro de IF: "+a+" - "+b);
+}
+console.log("Fuera de IF: "+a+" - "+b);
