e8118868db4b4966665a251cc87f76782eed6e6a
Ramsses <43966422+cramsses@users.noreply.github.com>
2021-01-04 21:03:44 -0600

    Herencia e Interfaces

--- curso-typescript/coche.js
+++ curso-typescript/coche.js
@@ -1,5 +1,14 @@
 var Coche = /** @class */ (function () {
-    function Coche() {
+    function Coche(modelo) {
+        if (modelo === void 0) { modelo = null; }
+        this.color = "BLANCO";
+        this.velocidad = 0;
+        if (modelo == null) {
+            this.modelo = "BMW Generico";
+        }
+        else {
+            this.modelo = modelo;
+        }
     }
     Coche.prototype.getColor = function () {
         return this.color;
@@ -7,8 +16,25 @@ var Coche = /** @class */ (function () {
     Coche.prototype.setColor = function (color) {
         this.color = color;
     };
+    Coche.prototype.getModelo = function () {
+        return this.modelo;
+    };
+    Coche.prototype.setModelo = function (modelo) {
+        this.modelo = modelo;
+    };
+    Coche.prototype.getVelocidad = function () {
+        return this.velocidad;
+    };
+    Coche.prototype.acelerar = function () {
+        this.velocidad++;
+    };
+    Coche.prototype.frenar = function () {
+        this.velocidad--;
+    };
     return Coche;
 }());
 var coche = new Coche();
 coche.setColor("AZUL");
+coche.acelerar();
 console.log("Color del cochecin: " + coche.getColor());
+console.log("Velocidad: " + coche.getVelocidad());
--- curso-typescript/coche.ts
+++ curso-typescript/coche.ts
@@ -1,20 +1,57 @@
-class Coche{
+interface CocheBase{
+    getModelo():string;
+    getVelocidad():number;
+}
+
+class Coche implements CocheBase{
     
     public color:string;
     public modelo:string;
     public velocidad:number;
 
+    constructor(modelo:any = null){
+        this.color = "BLANCO";
+        this.velocidad=0;
+        if(modelo == null){
+            this.modelo="BMW Generico";
+        }
+        else{
+            this.modelo=modelo;
+        }
+        
+    }
+
     public getColor(){
         return this.color;
     }
-
     public setColor(color:string){
         this.color=color;
+    }
+
+    public getModelo(){
+        return this.modelo;
+    }
+    public setModelo(modelo:string){
+        this.modelo=modelo;
+    }
+
+    public getVelocidad():number{
+        return this.velocidad;
+    }
+
+    public acelerar(){
+        this.velocidad++;
+    }
+
+    public frenar(){
+        this.velocidad--;
     }
 
 }
 
 var coche = new Coche();
 coche.setColor("AZUL");
+coche.acelerar();
 
 console.log("Color del cochecin: "+coche.getColor())
+console.log("Velocidad: "+coche.getVelocidad())
